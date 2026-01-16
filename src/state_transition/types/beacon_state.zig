6bee06ee7c17173c27f499f93cb4d98093f06967
Cayman <caymannava@gmail.com>
2026-01-16 08:17:01 -0500

    chore: apply suggestions from code review
    Co-authored-by: Chen Kai <281165273grape@gmail.com>

--- src/state_transition/types/beacon_state.zig
+++ src/state_transition/types/beacon_state.zig
@@ -1774,6 +1774,7 @@ pub const BeaconState = union(ForkSeq) {
                         var field_view = try committed_state.get(f.name);
                         const FieldView = @TypeOf(field_view);
                         const owned_field_view: FieldView = try field_view.clone(.{});
+                        errdefer owned_field_view.deinit();
                         try upgraded.set(f.name, owned_field_view);
                     }
                 } else {
@@ -1785,6 +1786,7 @@ pub const BeaconState = union(ForkSeq) {
                         var field_view = try committed_state.get(f.name);
                         const FieldView = @TypeOf(field_view);
                         const owned_field_view: FieldView = try field_view.clone(.{});
+                        errdefer owned_field_view.deinit();
                         try upgraded.set(f.name, owned_field_view);
                     }
                 }
