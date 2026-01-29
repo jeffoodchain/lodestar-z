a73e255cda396364c0a922bc2192561a275916a9
Cayman <caymannava@gmail.com>
2026-01-29 16:33:40 -0500

    chore: add syncPubkeysParallel

--- src/state_transition/cache/pubkey_cache.zig
+++ src/state_transition/cache/pubkey_cache.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const blst = @import("blst");
 const types = @import("consensus_types");
+const PublicKey = blst.PublicKey;
+const ValidatorIndex = types.primitive.ValidatorIndex.Type;
+const PubkeyIndexMap = @import("../utils/pubkey_index_map.zig").PubkeyIndexMap(ValidatorIndex);
 const Validator = types.phase0.Validator.Type;
 
 /// Map from pubkey to validator index
@@ -31,10 +34,14 @@ pub fn syncPubkeys(
     pubkey_to_index: *PubkeyIndexMap,
     index_to_pubkey: *Index2PubkeyCache,
 ) !void {
-    const old_len = index_to_pubkey.items.len;
-    if (pubkey_to_index.count() != old_len) {
-        return error.InconsistentCache;
+    if (pubkey_to_index.size() != index_to_pubkey.items.len) {
+        // TODO: is this a good pattern to debug?
+        std.debug.print("Error: Pubkey-to-index map size ({d}) does not match index-to-pubkey list length ({d})\n", .{ pubkey_to_index.size(), index_to_pubkey.items.len });
+        return error.InvalidPubkeyIndexMap;
     }
+
+    const old_len = index_to_pubkey.items.len;
+    try index_to_pubkey.resize(validators.len);
 
     const new_count = validators.len;
     if (new_count == old_len) {
@@ -45,9 +52,9 @@ pub fn syncPubkeys(
     try pubkey_to_index.ensureTotalCapacity(@intCast(new_count));
 
     for (old_len..new_count) |i| {
-        const pubkey = &validators[i].pubkey;
-        pubkey_to_index.putAssumeCapacity(pubkey.*, @intCast(i));
-        index_to_pubkey.items[i] = try blst.PublicKey.uncompress(pubkey);
+        const pubkey = validators[i].pubkey;
+        try pubkey_to_index.set(&pubkey, @intCast(i));
+        index_to_pubkey.items[i] = try PublicKey.uncompress(&pubkey);
     }
 }
 
